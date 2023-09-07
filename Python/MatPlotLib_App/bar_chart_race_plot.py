955c51da1e03d76e238c9b47cc34906c6fb4aa03
Thamizharasan-Gnanaprakasam <tamilarasan.g@gmail.com>
2023-09-07 15:23:47 -0400

    Update bar_chart_race_plot.py
    Working Bar Chart Race

--- Python/MatPlotLib_App/bar_chart_race_plot.py
+++ Python/MatPlotLib_App/bar_chart_race_plot.py
@@ -2,19 +2,25 @@ import pandas as pd
 import bar_chart_race as bcr
 import matplotlib.pyplot as plt
 
-#Documentation to install ffmpeg: https://trac.ffmpeg.org/wiki/CompilationGuide/macOS#ffmpegthroughHomebrew
+#Documentation to install ffmpeg: https://phoenixnap.com/kb/ffmpeg-mac
 
 languages = pd.read_csv("languages.csv",parse_dates=["Date"],index_col="Date")
 
+languages.index = languages.index.to_period("M")
+
 print(languages)
 
-bcr.bar_chart_race(languages.iloc[:10],
-                   n_bars=10,
-                   steps_per_period=130,
+bcr.bar_chart_race(languages, # DataFrame
+                   n_bars=10, # No. of Bars Should Be Shown
+                   steps_per_period=130, #Animation Speed
                    period_length=125,
                    title="Most Popular Language",
                    figsize=(6,4),
                    filter_column_colors=True,
+                   cmap=["forestgreen","skyblue"], # color
+                   dpi= 400,
+                   bar_label_size=5,#Values in Chart
+                   tick_label_size=10, #Y Axis Label
+                   filename="Sample.mp4"
                    )
 
-plt.show()
